2802e730304021df6964bc410863331f1f3e3c44
Tom Österlund <tom_jonathan.oesterlund@prosiebensat1.com>
2024-05-31 10:38:23 +0200

    various improvements

--- components/pages/landing-page.tsx
+++ components/pages/landing-page.tsx
@@ -38,7 +38,7 @@ export default function LandingPage() {
           </div>
 
           {/*<AppCalendar/>*/}
-          <Image className="landingPageDemoGif" src={'/videos/demo-premium.gif'} alt={'Demo'} width={800} height={250}/>
+          <Image id={'demo'} className="landingPageDemoGif" src={'/videos/demo-premium.gif'} alt={'Demo'} width={800} height={250}/>
         </section>
 
         <section id={'pricing'} className={'landingPage landingPage__pricing'}>
